4f44374b28e9255017ff8bf29498115f7e8809ce
Brayan Edgar Blanco Camacho <brayanbrx1@gmail.com>
2024-12-29 20:20:48 -0400

    practice

---
+++ Object.js
@@ -0,0 +1,50 @@
+class Persona{
+
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    nombreCompleto(){
+        return this._nombre + ' ' + this._apellido;
+    }
+
+}
+
+class Empleado extends Persona{
+    constructor(nombre, apellido,departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this._departamento;
+    }
+
+}
+
+
+let persona1 = new Persona('Juan', 'Perez');
+console.log(persona1.nombre);
+console.log(persona1.nombreCompleto());
+
+let empleado1 = new Empleado('Maria', 'Jimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombreCompleto());
+
+
+
+
+
---
+++ hola-mundo.js
@@ -0,0 +1,41 @@
+console.log("Hola Mundo...");
+
+let miNumero1 = 15;
+console.log(miNumero1);
+console.log(typeof miNumero1); //Para ver el tipo de dato de una variable
+const CONSTANTISIMO = 5;
+console.log(Math.PI);
+
+(miNumero1 > 0) ? console.log("positivo") : console.log("negativo"); //Op. Ternario
+
+let diaSemana = 3;
+switch (diaSemana){
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miercoles");
+        break;
+    default:
+        console.log("Dia Semana Erroneo")
+}
+let a = '10', b = '20'
+
+let cadena = 'Holanda';
+console.log(cadena.length);
+console.log(cadena.substring(0,4));
+console.log(cadena.substring(3));
+
+let res = parseInt(a) + parseInt(b)
+res = diaSemana.toString() + diaSemana.toString()
+console.log(res);
+
+
+
+
+
+
+
